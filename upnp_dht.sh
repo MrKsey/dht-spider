5924cbd02eef3e10e8a8997b8a1c2e621ae5beaa
ksey <17622530+MrKsey@users.noreply.github.com>
2022-05-02 22:22:58 +0300

    Update upnp_dht.sh

--- upnp_dht.sh
+++ upnp_dht.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
 
+echo " "
+echo "=================================================="
+echo "$(date): upnp_dht.sh started"
+echo "=================================================="
+echo " "
+
 # Try to passthrough DHT port 6881/UDP to container
 export DHT_PORT=6881
 export MY_IP=$(ip route | grep -E "default via" | cut -d ' ' -f 7)
 upnpc -d $DHT_PORT UDP
 upnpc -e "DHT spider" -a $MY_IP $DHT_PORT $DHT_PORT UDP
+
+echo " "
+echo "=================================================="
+echo "$(date): upnp_dht.sh finished"
+echo "=================================================="
+echo " "
