4a11cabb00170573f5e1748e19ae1d28a4338f7a
ksey <17622530+MrKsey@users.noreply.github.com>
2022-05-02 19:12:00 +0300

    Update upnp_dht.sh

--- upnp_dht.sh
+++ upnp_dht.sh
@@ -5,10 +5,3 @@ export DHT_PORT=6881
 export MY_IP=$(ip route | grep -E "default via" | cut -d ' ' -f 7)
 upnpc -d $DHT_PORT UDP
 upnpc -e "DHT spider" -a $MY_IP $DHT_PORT $DHT_PORT UDP
-UPNP_OK=$(upnpc -L | grep -o -s -E "UDP.*$DHT_PORT")
-if [ ! -z "$UPNP_OK" ]; then
-    echo "UPnP Port Forwarding complete: $UPNP_OK"
-else
-    echo "UPnP Port Forwarding failed."
-	echo "You must manually forward port $DHT_PORT to host $MY_IP on your router."
-fi
